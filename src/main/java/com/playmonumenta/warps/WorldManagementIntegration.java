e21ccb9fd4297b58f3505ad4dac88c61aa367d10
Byron Marohn <Combustible@live.com>
2023-01-03 03:59:46 +0000

    Minor breaking change: bump world management integration to 2.0+
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- src/main/java/com/playmonumenta/warps/WorldManagementIntegration.java
+++ src/main/java/com/playmonumenta/warps/WorldManagementIntegration.java
@@ -7,7 +7,7 @@ import org.bukkit.World;
 public class WorldManagementIntegration {
 	public static World getWorldFromIntegration(String worldName) {
 		try {
-			return MonumentaWorldManagementAPI.ensureWorldLoaded(worldName, false, false);
+			return MonumentaWorldManagementAPI.ensureWorldLoaded(worldName, null);
 		} catch (Exception ex) {
 			WarpsPlugin.getInstance().getLogger().warning("Failed to use world manager to get world '" + worldName + "': " + ex.getMessage());
 			ex.printStackTrace();
